8fef7a2bc516c25be5891d6583f49a4bd937ec1d
jesinmat <jesinmat@fit.cvut.cz>
2020-05-07 10:39:03 +0200

    Sort game servers alphabetically

--- overlay/usr/local/bin/gameserver-menu.py
+++ overlay/usr/local/bin/gameserver-menu.py
@@ -4,8 +4,7 @@ import sys
 import os
 from shutil import copy
 
-def main(games_dir):
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
+def parse_games_from_directory(games_dir):
     games_list = next(os.walk(games_dir))[1]
     games = []
     for dir in games_list:
@@ -17,20 +16,29 @@ def main(games_dir):
                         full_name = full_name[1:-1]
                     games.append((dir, full_name))
                     break
-    
+    return games
+
+def show_dialog(games):
     from dialog import Dialog
     d = Dialog(dialog="dialog")
+    games.sort(key=lambda game: game[1])
     choices = [(str(i)+")",game[1]) for i,game in enumerate(games)]
     _, gamenum = d.menu("Choose a game to install:",
                         title="Game Server Installer",
                         choices=choices,
                         no_cancel=True)
     selected_index = int(gamenum[0:-1])
-    selected_game = games[selected_index]
+    return games[selected_index]
+
+def main(gameservers_dir):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)  
+    games_dir = os.path.join(gameservers_dir, "games") 
+    games = parse_games_from_directory(games_dir)
+    selected_game = show_dialog(games)
     copy(os.path.join(games_dir, selected_game[0], "game_properties.sh"), "/etc/gameserver/gameserver")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Provide path to gameservers directory")
+        print("Provide path to gameservers repository directory!")
         exit(1)
     main(sys.argv[1])
