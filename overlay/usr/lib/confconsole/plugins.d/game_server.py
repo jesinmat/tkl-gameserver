7646be074513106efe8186d6726e445118c92106
jesinmat <jesinmat@fit.cvut.cz>
2020-07-27 15:45:43 +0200

    Add plugin text

--- overlay/usr/lib/confconsole/plugins.d/game_server.py
+++ overlay/usr/lib/confconsole/plugins.d/game_server.py
@@ -1,1 +1,16 @@
-'''Setup Game Server'''
+'''Select and install a gameserver'''
+import os
+
+def run():
+        if os.path.exists('/etc/gameserver/installation.done'):
+                console.msgbox('Info', 'Game Server is already installed.')
+                return
+
+        console.msgbox('Game Server Installer', 'Before you install a game server, new user account will be created for this purpose. '
+                       'Select a secure password for this account on the next screen. '
+                       'Do not select the same password as for the root account.\n\n'
+                       'Some game servers require basic configuration, such as setting the server name. '
+                       'You might be prompted for this info during the installation.')
+        os.system('gameserver-init')
+        console.msgbox('Info', 'Game Server was successfully installed.'
+                               'You can connect to this server using the IP address shown in the configuration console.')
